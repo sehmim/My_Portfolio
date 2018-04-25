53997460ca469c7099a89a0e544bddcfa8932bc1
sehmim <sehmim2010@gmail.com>
2018-04-25 01:58:26 -0400

    bin removed

--- app.js
+++ app.js
@@ -36,8 +36,12 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 5000);
   res.render('error');
 });
 
 module.exports = app;
+
+app.listen(3000, ()=>{
+  console.log("Listening on Port 3000")
+});
